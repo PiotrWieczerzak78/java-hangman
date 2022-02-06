f15559c9ce8fcc75a8056f3185ca14a2836cdfaa
tomal <tomal_l@poczta.onet.pl>
2022-02-06 15:11:57 +0100

    Part od CheckLetter is done

--- src/main/java/pl/edu/agh/hangman/InputLetter.java
+++ src/main/java/pl/edu/agh/hangman/InputLetter.java
@@ -3,15 +3,14 @@ package pl.edu.agh.hangman;
 import java.util.Scanner;
 
 public class InputLetter {
-    public static void main(String[] args) {
-
+    public String getChar() {
 
         Scanner input = new Scanner(System.in);
         System.out.println("Please enter a letter");
         String letter = input.nextLine();
         System.out.println(letter);
-        if(letter.length() > 1);
-        System.out.println("please put only one letter");
+        return letter;
+
 
 
     }
