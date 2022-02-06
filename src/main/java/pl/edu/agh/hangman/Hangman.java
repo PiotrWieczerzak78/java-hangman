bf6326586f68b34f1e5aa942d345cde7bdbe1d1d
Piotrek <piotr.pogorzelec@gazeta.pl>
2022-02-06 16:03:46 +0100

    zmiany w main

--- src/main/java/pl/edu/agh/hangman/Hangman.java
+++ src/main/java/pl/edu/agh/hangman/Hangman.java
@@ -60,17 +60,41 @@ public class Hangman {
     public static void main(String[] args) throws IOException {
         ReadFile readFile = new ReadFile();
         UI ui = new UI();
-         String filePath = "src/main/resources/slowa.txt";
+        InputLetter inputLetter = new InputLetter();
+        CheckLetter   checkLetter = new CheckLetter();
+        String filePath = "src/main/resources/slowa.txt";
         ArrayList<String> allWords = new ArrayList<>();
 
         allWords = readFile.fileReader(filePath);
 
         Randomizer randomizer = new Randomizer(allWords);
-        String wordToFind = randomizer.random();
+
 
         boolean endOfGame = false;
+        int iterator = 0;
+
 
         while (!endOfGame) {
+
+            String wordToFind = randomizer.random();
+
+
+            ui.displayCurrentState(HANGMANPICS, iterator);
+
+            ui.displayHiddenWord(wordToFind);
+
+            boolean letterGood;
+
+            for (int i=0; i < HANGMANPICS.length; i++) {
+
+                String word = inputLetter.getChar();
+
+
+                letterGood = checkLetter(word, wordToFind);
+            }
+
+
+
 
 
 
